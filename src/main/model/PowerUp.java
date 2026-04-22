0c8438d72967684a9106231ca02e860b3cb5b004
msobremisana <msobremisana@up.edu.ph>
2026-04-22 21:05:21 +0800

    added powerups and traps

---
+++ src/main/model/PowerUp.java
@@ -0,0 +1,36 @@
+package main.model;
+
+import java.awt.Rectangle;
+
+import java.util.Random;
+
+public class PowerUp {
+
+    public int x, y;
+    public int size = 20;
+
+    public enum Type {
+        SPEED, FREEZE, SHIELD, GHOST;
+    }
+
+    public Type type;
+
+    public PowerUp(int x, int y, Type type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+
+
+	public Rectangle getBounds() {
+        return new Rectangle(x, y, size, size);
+    }
+
+    public static Type getRandomType() {
+        Type[] types = Type.values();
+        return types[new Random().nextInt(types.length)];
+    }
+    
+    
+}
